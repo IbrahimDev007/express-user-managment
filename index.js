8a459ad2f90dd7644e4e00a2623cbfd6088828d1
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-05-07 21:30:31 +0600

    complete

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 const user = [
     { id: 1, name: "jon", email: "jon@email.com" },
     { id: 2, name: "ron", email: "ron@email.com" },
@@ -9,12 +9,22 @@ const user = [
     { id: 4, name: "fon", email: "fon@email.com" },
 ]
 app.use(cors())
-
+app.use(express.json())
 app.get('/', (req, res) => {
     res.send('user managment server is running')
 })
 app.get('/users', (req, res) => {
     res.json(user)
+
+})
+app.post('/users', (req, res) => {
+    console.log('post api hitting');
+
+    console.log(req.body)
+    const newUser = req.body;
+    newUser.id = user.length + 1
+    user.push(newUser);
+    res.send(newUser);
 
 })
 
